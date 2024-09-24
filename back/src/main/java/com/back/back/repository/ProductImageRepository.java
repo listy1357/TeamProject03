d52544f2d0cdcf7221fce09b6817d22892deb85c
listy1357 <listy1357@naver.com>
2024-09-24 16:34:01 +0900

    Removed ignored files

--- back/src/main/java/com/back/back/repository/ProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.back.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.back.back.entity.ProductImage;
-
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-    // 필요 시, 이미지 관련 쿼리를 추가
-}
